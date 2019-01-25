bf9d05f5fd5976bb37aedf36b8ab9bca9b853c36
NANSONG YI <nansong@uw.edu>
2019-01-25 14:17:11 -0800

    yes

--- robot_api/src/robot_api/base.py
+++ robot_api/src/robot_api/base.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 
+<<<<<<< HEAD
 # TODO: import ????????_msgs.msg
+=======
+>>>>>>> 7387faa... yes
 from geometry_msgs.msg import Twist
 import rospy
 
@@ -9,16 +12,20 @@ class Base(object):
     """Base controls the mobile base portion of the Fetch robot.
 
     Sample usage:
-        base = fetch_api.Base()
+        base = fetch_api.Base()s
         while CONDITION:
             base.move(0.2, 0)
         base.stop()
     """
 
     def __init__(self):
+<<<<<<< HEAD
         # TODO: Create publisher
         self.pub = rospy.Publisher("/mobile_base_controller/cmd_vel", Twist, queue_size = 10)
         #rospy.init_node('base', anonymous = True)
+=======
+        self.pub = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist, queue_size=10)
+>>>>>>> 7387faa... yes
 
     def move(self, linear_speed, angular_speed):
         """Moves the base instantaneously at given linear and angular speeds.
@@ -32,6 +39,7 @@ class Base(object):
             angular_speed: The rotation speed, in radians/second. A positive
                 value means the robot should rotate clockwise.
         """
+<<<<<<< HEAD
         # TODO: Create Twist msg
         msg = Twist()
         msg.linear.x = linear_speed
@@ -39,10 +47,24 @@ class Base(object):
         self.pub.publish(msg)
         # TODO: Fill out msg
         # TODO: Publish msg
+=======
+        msg = Twist()
+        msg.linear.x = linear_speed
+        msg.angular.z = angular_speed 
+        self.pub.publish(msg)
+        # TODO: Fill out msg
+        # TODO: Publish msg
+        # rospy.logerr('Not implemented.')
+>>>>>>> 7387faa... yes
 
     def stop(self):
         """Stops the mobile base from moving.
         """
+<<<<<<< HEAD
         # TODO: Publish 0 velocity
         self.move(0,0)
 
+=======
+        self.move(0,0)
+        # rospy.logerr('Not implemented.')
+>>>>>>> 7387faa... yes
