407d4561acd28db1caca69189cd646b82a36a636
team5 desktop <team5>
2019-01-29 20:14:49 -0800

    fix data structure

--- applications/scripts/marker_demo.py
+++ applications/scripts/marker_demo.py
@@ -11,12 +11,12 @@ class NavPath(object):
         self._pub = pub
         self._pre_point = Point(x=0,y=0,z=0)
         self._path = []
-        self._path.insert(0, [self._pre_point])
+        self._path.insert(0, self._pre_point)
         rospy.logerr(self._pre_point)
 
     def dist(self, p1, p2):
         dp = [p1.x - p2.x, p1.y - p2.y, p1.z - p2.z]
-        rospy.loginfo(np.linalg.norm(np.array(dp)))
+        # rospy.loginfo(np.linalg.norm(np.array(dp)))
         return np.linalg.norm(np.array(dp))
 
     def callback(self, msg):
@@ -25,38 +25,40 @@ class NavPath(object):
         pre_points = self._pre_point
         if self.dist(now_points, pre_points) > 0.2:
             rospy.logerr("hi ready to pub")
-            # self._path.append(msg.pose.pose.position)
-            self._path[0] = now_points
+            self._path.append(now_points)
+            # self._path[0] = now_points
             rospy.logerr(self._path)
             self._pre_point = now_points
             now_marker = Marker(
                 type=Marker.LINE_STRIP,
                 id=len(self._path),
-                lifetime=rospy.Duration(10),
-                action = Marker.ADD,
-                # scale=Vector3(0.06, 0.06, 0.06),
-                header=Header(frame_id='point', stamp = rospy.Time.now()),
+                ns="path",
+                lifetime=rospy.Duration(60),
+                action=Marker.ADD,
+                scale=Vector3(0.06, 0.06, 0.06),
+                pose=Pose(now_points, Quaternion(0, 0, 0, 1)),
+                header=Header(frame_id='base_link', stamp = rospy.Time.now()),
                 color=ColorRGBA(1.0, 0, 1.0, 0.8),
                 points=self._path)
             self._pub.publish(now_marker)
 
 
 def wait_for_time():                                              
-    """Wait for simulated time to begin.                          
-    """                                                           
-    while rospy.Time().now().to_sec() == 0:                       
+    """Wait for simulated time to begin.
+    """
+    while rospy.Time().now().to_sec() == 0:
         pass
 
 def show_text_in_rviz(marker_publisher, text):
     marker = Marker(
-                type=Marker.TEXT_VIEW_FACING,
-                id=0,
-                lifetime=rospy.Duration(10),
-                pose=Pose(Point(0.5, 0.5, 1.45), Quaternion(0, 0, 0, 1)),
-                scale=Vector3(0.06, 0.06, 0.06),
-                header=Header(frame_id='base_link'),
-                color=ColorRGBA(1.0, 0, 1.0, 0.8),
-                text=text)
+        type=Marker.TEXT_VIEW_FACING,
+        id=0,
+        lifetime=rospy.Duration(1000),
+        pose=Pose(Point(0.5, 0.5, 1.45), Quaternion(0, 0, 0, 1)),
+        scale=Vector3(0.06, 0.06, 0.06),
+        header=Header(frame_id='base_link'),
+        color=ColorRGBA(1.0, 0, 1.0, 0.8),
+        text=text)
     marker_publisher.publish(marker)
 
 def main():
