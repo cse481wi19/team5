b41d81587971e9256e3c8e2a1cf332629590e1a1
team5 desktop <team5>
2019-03-09 16:56:52 -0800

    update dictionary

--- applications/nodes/ava_voice_cmd.py
+++ applications/nodes/ava_voice_cmd.py
@@ -38,25 +38,25 @@ class AvaVoiceCommand:
         self.social_cues = robot_api.Social_Cues()
         self.lights = robot_api.Lights()
         self.sound_src = kuri_api.SoundSource('AvaVoiceCommand')
+        self.sound_src_music = kuri_api.SoundSource('AvaVoiceCommand-music')
+        
+        self._kill_prompt_thread = False
+        threading.Thread(target=self._prompt_thread).start()
+        rospy.on_shutdown(self.cleanup)
 
         self._log_set_state(REST)
         self._sound_dir = os.getcwd() + "/../catkin_ws/src/cse481wi19/ava_custom_audio/"
 
-        # publish to cmd_vel, subscribe to speech output
+        # publish to cmd_vel (publishes empty twist message to stop), subscribe to speech output
         self.pub_ = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         rospy.Subscriber('recognizer/asr_output', String, self.speechCb)
         rospy.spin()
-
-        # r = rospy.Rate(10.0)
-        # while not rospy.is_shutdown():
-        #     self.pub_.publish(self.msg)
-        #     r.sleep()
 
     def promptUser(self):
         """ Proactively prompts user, asking "Are you feeling okay?" """
         self._state = PROMPT
         self.wait_play_sound(self._sound_dir + "/FeelingOk.wav")
-        self._abort_after(10)
+        self._abort_after(20)
 
     def speechCb(self, msg):
         if not msg.data:
@@ -82,12 +82,14 @@ class AvaVoiceCommand:
                 self.social_cues.express_sad()
                 # say Good night, see you tomorrow
                 self.wait_play_sound(self._sound_dir + "/GoodNight.wav")
+                #TODO go back to charging station, close eyes
 
-            elif "THANK" in msg.data:
+            elif "THANK YOU" in msg.data:
                 self.social_cues.express_happy()
             
-            elif "FAVORITE SONG" in msg.data or "MY SONG" in msg.data:
-                self.wait_play_sound(self._sound_dir + "/just_breathe.wav")
+            elif "PLAY" in msg.data and \
+                    ("FAVORITE SONG" in msg.data or "MY SONG" in msg.data):
+                self.wait_play_music(self._sound_dir + "/just_breathe.wav")
                 self.social_cues.nod_head()
 
             elif "YOUR BACKPACK" in msg.data:
@@ -96,39 +98,40 @@ class AvaVoiceCommand:
         elif self._state is STRESS_ASK:
             if "YOUR BACKPACK" in msg.data:
                 self._exe_backpack()   # --> state BACKPACK
-            elif "YES THANK" in msg.data:
+            elif "YES PLEASE" in msg.data:
                 self._log_set_state(STRESS_HELP)
                 self._exe_help_msg()
-            elif "NO THANK" in msg.data:
+            elif "NO THANKS" in msg.data:
                 # say okay, feel free to ask whenever
                 self.wait_play_sound(self._sound_dir + "/AskWhenever.wav")
                 self.social_cues.nod_head()
                 self._log_set_state(REST)
             
         elif self._state is STRESS_HELP:
-            if "I'M NOT" in msg.data:
+            if "NOT REALLY" in msg.data:
                 self._exe_help_msg()
                 # don't change state
-            elif "FEELING BETTER" in msg.data \
-                    or "FEELING GOOD" in msg.data \
-                    or "FEELING HAPPY" in msg.data \
-                    or "THANK" in msg.data:
+                self._log_set_state(STRESS_HELP)
+            elif "YES THANKS" in msg.data \
+                    or "YES THANK YOU" in msg.data:
                 self.wait_play_sound(self._sound_dir + "/bastion_hello_loud.wav")
                 self._log_set_state(REST)
 
         elif self._state is BACKPACK:
             if "THANK" in msg.data \
                     or "DONE" in msg.data:
-                # turn around, back up a little
+                # TODO: turn around, back up a little
                 self.social_cues.express_happy()
                 self._log_set_state(REST)
         
         elif self._state is PROMPT:
             if "YES" in msg.data \
-                    or "OKAY" in msg.data:
-                #do stuff
+                    or "I'M OKAY" in msg.data:
+                self.wait_play_sound(self._sound_dir + "/AskWhenever.wav")
+                self.social_cues.nod_head()
+                self._log_set_state(REST)
             elif "NO" in msg.data:
-                #do stuff
+                self._exe_offer_help()
 
         else:
             rospy.logerr("VOICE INTERACTION: ILLEGAL STATE")
@@ -142,26 +145,26 @@ class AvaVoiceCommand:
         self.social_cues.express_sad()
         self.wait_play_sound(self._sound_dir + "/HelpOrBackpack.wav")
         self.wait_play_sound(self._sound_dir + "/bastion_confuse_loud.wav")
-        self._abort_after(10)
+        self._abort_after(20)
 
     def _exe_help_msg(self):
-        #self.wait_play_sound(self._sound_dir + "/just_breathe_quiet.wav")
         # play two random help messages over music
+        self.wait_play_music(self._sound_dir + "/just_breathe_quiet.wav")
         msg1 = random.randrange(len(ENCOURAGEMENTS))
         msg2 = (msg1 + random.randrange(len(ENCOURAGEMENTS)-1) + 1) % len(ENCOURAGEMENTS)
         self.wait_play_sound(self._sound_dir + ENCOURAGEMENTS[msg1])
         self.wait_play_sound(self._sound_dir + ENCOURAGEMENTS[msg2])
-        rospy.sleep(5)
-        # ask are you feeling better
+        # ask are you feeling better.
+        rospy.sleep(10)
         self.wait_play_sound(self._sound_dir + "/HelpMore.wav")
-        self._abort_after(10)
+        self._abort_after(20)
 
     def _exe_backpack(self):
+        self.wait_play_sound(self._sound_dir + "/OnMyWay.wav")
         self.social_cues.nod_head()
-        #self.wait_play_sound(self._sound_dir + "/OnMyWay.wav")
         # TODO move to user and turn around
         self._log_set_state(BACKPACK)
-        self._abort_after(30)
+        self._abort_after(60)
 
     def _abort_after(self, n):
         """ aborts after n seconds. spins a new thread to do the spinning """
@@ -170,17 +173,19 @@ class AvaVoiceCommand:
     def _abort_after_thread(self, n, state):
         rospy.loginfo("ABORT THREAD STARTED: waiting on STATE=" + str(state))
         ring = [self.lights.OFF] * self.lights.NUM_LEDS
+        d = rospy.Duration(nsecs=0.5 * 1e9)
         for i in self.lights.LED_OUTER_RING:
             ring[i] = self.lights.BLUE
-        for x in range(2*n):
+        for _ in range(n):
             # pulse lights
             if self._state is not state:
                 # if cb thread heard another utterance then stop pulsing light
                 break
             self.lights.put_pixels(ring)
-            rospy.sleep(0.25)
+            rospy.sleep(d)
             self.lights.all_leds(self.lights.OFF)
-            rospy.sleep(0.25)
+            rospy.sleep(d)
+
         if self._state is state:
             rospy.loginfo("ABORT INTERACTION: STATE RESET TO REST")
             self._log_set_state(REST)
@@ -192,6 +197,15 @@ class AvaVoiceCommand:
         # ask are you feeling okay (use a different method)
         # some private var for self._should_prompt (don't prompt at night)
         # change state/call exe_offer_help if no, nothing if yes
+        rospy.loginfo("PROMPT THREAD STARTED")
+        d = rospy.Duration(secs=180)
+        rospy.sleep(5)
+        while not self._kill_prompt_thread:
+            # TODO: if {am_following_user} and state is rest, then prompt:
+            if self._state is REST:
+                self.promptUser()
+            rospy.sleep(d)
+        rospy.loginfo("PROMPT THREAD KILLED")
 
     def _log_set_state(self, state):
         self._state = state
@@ -199,12 +213,20 @@ class AvaVoiceCommand:
 
     def wait_play_sound(self, wavfile):
         while self.sound_src.is_playing:
-            rospy.sleep(0.25)
+            rospy.sleep(0.5)
         self.sound_src.play(wavfile)
+    
+    def wait_play_music(self, wavfile):
+        while self.sound_src_music.is_playing:
+            rospy.sleep(0.5)
+        self.sound_src_music.play(wavfile)
 
     def cleanup(self):
         # stop the robot!
         self.pub_.publish(self.msg)
+        self._kill_prompt_thread = True
+        self.lights.all_leds(self.lights.OFF)
+
 
 if __name__=="__main__":
     AvaVoiceCommand()
